1dcdc1f6ec43ddd269b48ea017297a363bf891b1
Wilmer Krisp <wilmer.krisp@yahoo.com>
2020-03-22 14:00:17 +0300

    PY-2019.3 <wilmer@WILMERBOARD Create ide.general.xml, _TYPES.xml, Windows copy.xml, Default.xml, _ITER.xml, laf.xml, _LOG.xml, jdk.table.xml, _FUNCS.xml, Default.xml, find.xml, wsl.distributions.xml, filetypes.xml, keymap.xml, Python Script.py, colors.scheme.xml, _BASE.xml, _TEST.xml, _COLL.xml, editor.xml, templates.xml, debugger.xml, layout.xml, updates.xml

---
+++ fileTemplates/internal/Python Script.py
@@ -0,0 +1,4 @@
+from collections import *
+from typing import * #TypeVar, Iterable, Tuple, Union, Generic, Sized,NewType
+from functools import * #reduce
+from operator import * #add - lambda functions
